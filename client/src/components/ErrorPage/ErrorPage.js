4d2aa5982af32f2ed02863390e346e4c6db08176
Lobo <matheuscardoso1@poli.ufrj.br>
2021-05-23 18:59:29 -0300

    added an error page, in case something goes wrong

---
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from "./ErrorPage.module.css";
+
+import React, { Component } from "react";
+
+class ErrorPage extends Component {
+  state = {};
+  render() {
+    return <h1>Oops, parece que houve um erro!</h1>;
+  }
+}
+
+export default ErrorPage;
